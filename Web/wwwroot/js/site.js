eff9f6a9da03bc58f0ceefb06e1a26934ddea012
Samir Habota <habota.mail@gmail.com>
2019-12-21 22:53:07 +0100

    card css, js rotation, card html

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -6,3 +6,30 @@
 $(document).ajaxComplete(function () {
 
 });
+
+function flip() {
+    var card = document.getElementById("cardId");
+    var isFLipped = card.getAttribute("isFlipped");
+    if (isFLipped == 'true') {
+        card.classList.add("flipped");
+        setTimeout(function () {
+            document.getElementById("hintId").setAttribute("style", "display:none");
+            document.getElementById("showAnswerId").setAttribute("style", "display:none");
+        }, 300);
+        card.setAttribute("isFlipped", "false");
+    }
+    else {
+        card.classList.remove("flipped");
+        setTimeout(function () {
+            document.getElementById("hintId").removeAttribute("style", "display:none");
+            document.getElementById("showAnswerId").removeAttribute("style", "display:none");
+        }, 300);
+        card.setAttribute("isFlipped", "true");
+    }
+}
+
+document.body.onkeyup = function (e) {
+    if (e.keyCode == 32) {
+        flip();
+    }
+}   

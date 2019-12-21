1cabf76bf1afa3514623de0ddb1b3dcf2efb3582
Adi-Sose <adi.sose@outlook.com>
2019-12-21 10:12:05 +0100

    Initial

---
+++ Web/Viewmodels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web.Viewmodels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
